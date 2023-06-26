3d9fcc958e80be5a23f9811810a294313b9cfee8
lints123 <xxx@qq.com>
2023-06-27 00:53:05 +0800

    [ioc] IOC学习

--- spring-aop/readme.txt
+++ spring-aop/readme.txt
@@ -2,3 +2,5 @@
 1. XML配置 - MyActive
 2. XML配置和注解方式配置 - MyActive2
 3. 全注解方式配置 - MyActive3
+
+学习网址: https://www.bilibili.com/video/BV1rt4y1u7q5?p=102&spm_id_from=pageDriver&vd_source=84ffac2d3bf7e4396096fc5d76a666b8
---
+++ spring-ioc/src/main/java/com/learn/processer/MyBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,24 @@
+package com.learn.processer;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+/**
+ * @author lints
+ * @Date 2023/6/27
+ * @Descirption  注册BeanDefinition
+ */
+public class MyBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+
+
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+        System.out.println("MyBeanDefinitionRegistryPostProcessor >>> postProcessBeanDefinitionRegistry >>> ");
+        // 注册BeanDefinition
+    }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
+}
